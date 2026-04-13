bffd4a86ab3b9fd5f7963f7a434f67259a492c1a
Kartik Panganti <kartikpanganti@gmail.com>
2026-04-13 23:27:19 +0530

    update token limit

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ model = GoogleGenerativeAI(
     temperature=0.7,
     top_p=1,
     top_k=1,
-    max_output_tokens=4096,
+    max_output_tokens=2048,
 )
 # promt template
 prompt_template_resto = PromptTemplate(
